155cddb06651acde89c1e976c36ad1c06d5a259e
JonMS95 <jon_portugalete@hotmail.com>
2022-12-25 18:35:56 +0100

    Added some more extra code, changed C include files to relative.

--- Source_files/C++/main.cpp
+++ Source_files/C++/main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <vector>
+
+#define FIB_NUMBER_AMOUNT   10
 
 extern "C" 
 {
-    #include "/home/jon/Desktop/scripts/C++/CPP_Use_Programs_in_C/Source_files/C/fibonacciNumber.h"
+    #include "../C/fibonacciNumber.h"
 }
 
 int main()
 {
+    std::vector<int> fibSeq(FIB_NUMBER_AMOUNT);
+    int fibNumber;
+
     for(int i = 0; i < 10; i++)
     {
+        fibNumber = fibNum(i);
         std::cout << fibNum(i) << "\n";
+        fibSeq[fibNumber];
     }
 
     return 0;
