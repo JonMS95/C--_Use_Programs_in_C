1ef2483cf9a8670606e9af62d8becef186435194
JonMS95 <jon_portugalete@hotmail.com>
2022-12-25 17:48:27 +0100

    Added conditional guards for both .h files.

--- Source_files/C/fibonacciNumber.h
+++ Source_files/C/fibonacciNumber.h
@@ -1,1 +1,6 @@
+#ifndef FIBONACCI_NUMBER_H
+#define FIBONACCI_NUMBER_H
+
 int fibNum(int n);
+
+#endif
--- Source_files/C/swapIntegers.h
+++ Source_files/C/swapIntegers.h
@@ -1,1 +1,6 @@
+#ifndef SWAP_INTEGERS_H
+#define SWAP_INTEGERS_H
+
 void swapInt(int* a, int* b);
+
+#endif
